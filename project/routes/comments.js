13a7f01ab7741f623d018bc125d86784730b865f
elit <elitmanes@gmail.com>
2022-01-10 18:15:48 +0200

    label comments only

--- project/routes/comments.js
+++ project/routes/comments.js
@@ -12,5 +12,20 @@ router.get("/getCommentsOfPost/:postId", async (req, res, next) => {
     }
   });
 
+  router.post("/labelcomments", async (req, res, next) => {
+    console.log(req.body.commentId);
+    console.log(req.body.with_politition);
+    try {
+       await commentsUtils.labelcomments(req.body.commentId, req.session.username,req.body.pre_comment ,req.body.with_politition,
+        req.body.against_politition, req.body.with_party, req.body.against_party,
+        req.body.with_group, req.body.against_group,req.body.not_authentic,
+        req.body.why_not_authentic, req.body.free_text);
+        res.status(200).send();
+    } catch (error) {
+      next(error);
+    }
+  });
+
+
 
   module.exports = router;
--- project/routes/utils/commentsUtils.js
+++ project/routes/utils/commentsUtils.js
@@ -1,4 +1,6 @@
 const DButils = require("./DButils");
+const { type } = require("express/lib/response");
+
 
 async function getCommentsOfPost(post_id) {
     try{
@@ -13,4 +15,28 @@ catch (err) {
   }
 }
 
+async function labelcomments(commentId, username, pre_comment ,with_politition , against_politition, 
+  with_party, against_party, with_group,against_group,not_authentic,
+  why_not_authentic, free_text){
+  try{  
+  await DButils.execQuery(
+      `INSERT INTO labeledcomments VALUES ('${commentId}', '${username}','${pre_comment}', array[${arrFormat(with_politition)}],
+      array[${arrFormat(against_politition)}] ,array[${arrFormat(with_party)}],
+      array[${arrFormat(against_party)}],array[${arrFormat(with_group)}],
+      array[${arrFormat(against_group)}], '${not_authentic}','${why_not_authentic}','${free_text}')`
+    );
+  }
+catch (err) {
+  console.error("SQL error", err);
+  throw err;
+}}
+
+function arrFormat(arr){
+  if (typeof(arr) == 'string')
+    return `'${arr}'`;
+  else
+    return arr.map(str => `'${str}'`).join(',');
+}
+
 exports.getCommentsOfPost = getCommentsOfPost;
+exports.labelcomments = labelcomments;
